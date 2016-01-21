59c59a282da9480bdfd9e541195887015babe64e
RomanAngelovskij <turbocom-mlt@yandex.ru>
2016-01-21 19:21:21 +0300

    fixed path to css

--- app/views/layout/main.php
+++ app/views/layout/main.php
@@ -6,7 +6,7 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
-	<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
 
 	<title>FM</title>
 </head>
